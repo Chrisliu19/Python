2ba8e155c43e95f24053837045bc02fe32b2aa38
Chrisliu19 <529286787@qq.com>
2017-08-30 19:54:20 +0800

    新项目-餐厅自动化程序

---
+++ 餐厅自动化程序.py
@@ -0,0 +1,23 @@
+#__author__ = 'Chris19'
+
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+Topping = ['pepperoni','sausage','cheese']
+
+item1 = raw_input('Please give me a topping: ')
+item2 = raw_input('Please give me one more topping: ')
+Topping2 = []
+
+print '\nYou can check the topping in this Table'
+print '-' * 40
+
+if item1 in Topping:
+    Topping2.append(item1)
+elif item2 in Topping:
+    Topping2.append(item2)
+else:
+    print '\nSorry,we don\'t have these toppings'
+
+last = 'Here are your toppings: {}'
+print last.format(Topping2)
